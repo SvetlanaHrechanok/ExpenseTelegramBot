dba95235e264b07cc08bdea4dae9c206151d88ca
Svetlana <svetochkaufo@yandex.ru>
2019-11-18 11:39:21 +0300

    init

--- src/index.js
+++ src/index.js
@@ -9,3 +9,5 @@ bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
+
+console.log('HALLO!!!');
