33fcd1665360a4cf05116c0c6a87f13ca7d64854
Svetlana <svetochkaufo@yandex.ru>
2019-11-18 13:02:46 +0300

    init

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
-const helper = require('./helper');
-const config = require('./config');
+//const helper = require('./helper');
+//const config = require('./config');
 const Telegraf = require('telegraf');
-const server = require('./server');
+//const server = require('./server');
 
-const bot = new Telegraf(config.TOKEN);
+// const bot = new Telegraf(config.TOKEN);
+const bot = new Telegraf('971500404:AAFNDLpv3OjhngQPNgJJkR2nALaw2LVWXNg');
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
