eff31aed7827ea38df9dcc7d4fe978d39da5d1e5
Svetlana <svetochkaufo@yandex.ru>
2019-11-19 10:14:24 +0300

    init

--- src/config.js
+++ src/config.js
@@ -1,8 +1,17 @@
 module.exports = {
-    TOKEN: `971500404:AAFNDLpv3OjhngQPNgJJkR2nALaw2LVWXNg`,
-    CONSUMER_KEY: `3MVG9G9pzCUSkzZsPquVIgSFvVZSjEh72I3mGHn86gf0xL3EGGUdhStgD_JCwghbUOzqmkbFKnL16ogwjqsPu`,
-    CONSUMER_SECRECT: `A041CDC76815A831A291BA23156817BA2F468B64EC5E2E2B2DC743682B73CCEA`,
-    SFUSER: `expensecard@expensecard.com`,
-    SFPASS: `svetacraft1985`,
-    SECURITY_TOKEN: `4xn8k1Obz5FBRjDOFKTCpuMBP`
+    bot: {
+        TOKEN: `971500404:AAFNDLpv3OjhngQPNgJJkR2nALaw2LVWXNg`
+    },
+    salesforce: {
+        CONSUMER_KEY: `3MVG9G9pzCUSkzZsPquVIgSFvVZSjEh72I3mGHn86gf0xL3EGGUdhStgD_JCwghbUOzqmkbFKnL16ogwjqsPu`,
+        CONSUMER_SECRECT: `A041CDC76815A831A291BA23156817BA2F468B64EC5E2E2B2DC743682B73CCEA`,
+        SFUSER: `expensecard@expensecard.com`,
+        SFPASS: `svetacraft1985`,
+        SECURITY_TOKEN: `4xn8k1Obz5FBRjDOFKTCpuMBP`
+    },
+    heroku: {
+        URL: `https://stormy-wave-90920.herokuapp.com/`
+    },
+    http: 3000,
+    https: 8443
 }
--- src/index.js
+++ src/index.js
@@ -2,20 +2,42 @@ const nforce = require('nforce');
 const Telegraf = require('telegraf');
 const helper = require('./helper');
 const config = require('./config');
+const session = require('telegraf/session');
+const Stage = require('telegraf/stage');
+const Scene = require('telegraf/scenes/base');
+const { leave } = Stage;
+const stage = new Stage();
 
 //nforce setup to connect Salesforce
 const org = nforce.createConnection({
-    clientId: config.CONSUMER_KEY,
-    clientSecret: config.CONSUMER_SECRECT,
-    redirectUri: 'https://stormy-wave-90920.herokuapp.com/',
+    clientId: config.salesforce.CONSUMER_KEY,
+    clientSecret: config.salesforce.CONSUMER_SECRECT,
+    redirectUri: config.heroku.URL,
     environment: 'production',
     mode: 'single'
 
 });
+let oauth,
+    port = config.http || config.https;
+org.authenticate({ username: config.salesforce.SFUSER, password: config.salesforce.SFPASS, securityToken: config.salesforce.SECURITY_TOKEN }, function(err, resp){
+    if(!err) {
+        oauth = resp;
+        console.log('Connection is completed');
+    } else {
+        console.log('Error: ' + err.message);
+    }
+});
 
-const bot = new Telegraf(config.TOKEN);
+const bot = new Telegraf(config.bot.TOKEN);
+bot.telegram.setWebhook(`${config.heroku.URL}bot${config.bot.TOKEN}`);
+bot.startWebhook(`/bot${config.bot.TOKEN}`, null, port);
+bot.use(session());
+bot.use(stage.middleware());
+
 bot.start((ctx) => ctx.reply('Welcome'));
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
 bot.launch();
+
+
