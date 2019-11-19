da37e687707984ec2c1be9b9e56914a4b7b8dffd
Svetlana <svetochkaufo@yandex.ru>
2019-11-19 11:27:30 +0300

    init

--- src/index.js
+++ src/index.js
@@ -34,7 +34,48 @@ bot.startWebhook(`/bot${config.bot.TOKEN}`, null, port);
 bot.use(session());
 bot.use(stage.middleware());
 
-bot.start((ctx) => ctx.reply('Welcome'));
+const login = new Scene('login');
+const password = new Scene('password');
+
+// Create scene manager
+stage.register(login);
+stage.register(password);
+
+bot.start((ctx) => {
+    ctx.reply(`Welcome`);
+    ctx.scene.enter('login');
+});
+bot.catch((err, ctx) => {
+    console.log(`Ooops, ecountered an error for ${ctx.updateType}`, err)
+});
+
+//Login scene
+login.enter((ctx) => {
+    ctx.reply(`${ctx.from.first_name}, enter your login: `);
+});
+login.on('text', (ctx) => {
+    ctx.session.login = ctx.message.text;
+    ctx.scene.enter('password');
+});
+
+//password scene
+password.enter((ctx) => {
+    ctx.reply(`${ctx.session.login}, enter your password: `);
+});
+password.on('text',async (ctx) => {
+    ctx.session.password = ctx.message.text;
+    let query = `SELECT Id, Name, Email FROM Contact WHERE Email =` + `'` + `${ctx.session.login}` + `'` + ` AND Password__c =` + `'` + `${ctx.session.password}` + `'`;
+    org.query({ query: query }, async (err, resp) => {
+        if (!err && resp.records) {
+            let contact = JSON.parse(JSON.stringify(resp.records[0]));
+            ctx.session.userId = contact.Id;
+            ctx.session.userName = contact.Name;
+            ctx.reply(`Success!`).then(()=>ctx.scene.enter('login'));
+        } else {
+            ctx.reply('Invalid login or password!!!\nTry again!').then(()=>ctx.scene.enter('login'));
+        }
+    });
+});
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
