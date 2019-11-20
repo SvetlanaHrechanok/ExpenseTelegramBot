023edf66cef88788e7bebe071f76998d506130a2
Svetlana <svetochkaufo@yandex.ru>
2019-11-20 11:34:34 +0300

    init

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,40 @@
-module.exports = {
+const config = require('./config');
+const nforce = require('nforce');
+let http = require('http');
 
+//nforce setup to connect Salesforce
+const org = nforce.createConnection({
+    clientId: config.salesforce.CONSUMER_KEY,
+    clientSecret: config.salesforce.CONSUMER_SECRECT,
+    redirectUri: config.heroku.URL,
+    environment: 'production',
+    mode: 'single'
+
+});
+
+
+module.exports = {
+    conectOrg: org,
+
+    formatDate(date) {
+        let dd = date.getDate();
+        if (dd < 10) dd = '0' + dd;
+        let mm = date.getMonth() + 1;
+        if (mm < 10) mm = '0' + mm;
+        let yy = date.getFullYear() % 100;
+        if (yy < 10) yy = '0' + yy;
+
+        return dd + '.' + mm + '.' + yy;
+    },
+    getHttp() {
+        http.get('http://psk-tgbot.herokuapp.com/');
+        org.authenticate({ username: orgUserName, password: orgPassword, securityToken: securityToken}, function(err, resp){
+            if(!err) {
+                oauth = resp;
+                console.log('Connection complete');
+            } else {
+                console.log('Error: ' + err.message);
+            }
+        });
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const nforce = require('nforce');
 const Telegraf = require('telegraf');
+const Markup = require('telegraf/markup');
 const helper = require('./helper');
 const config = require('./config');
 const session = require('telegraf/session');
@@ -8,18 +9,12 @@ const Scene = require('telegraf/scenes/base');
 const { leave } = Stage;
 const stage = new Stage();
 
-//nforce setup to connect Salesforce
-const org = nforce.createConnection({
-    clientId: config.salesforce.CONSUMER_KEY,
-    clientSecret: config.salesforce.CONSUMER_SECRECT,
-    redirectUri: config.heroku.URL,
-    environment: 'production',
-    mode: 'single'
-
-});
 let oauth,
-    port = config.http || config.https;
-org.authenticate({ username: config.salesforce.SFUSER, password: config.salesforce.SFPASS, securityToken: config.salesforce.SECURITY_TOKEN }, function(err, resp){
+    port = config.http || config.https,
+    state = {},
+    conectOrgSF = helper.conectOrg;
+
+conectOrgSF.authenticate({ username: config.salesforce.SFUSER, password: config.salesforce.SFPASS, securityToken: config.salesforce.SECURITY_TOKEN }, function(err, resp){
     if(!err) {
         oauth = resp;
         console.log('Connection is completed');
@@ -34,24 +29,28 @@ bot.startWebhook(`/bot${config.bot.TOKEN}`, null, port);
 bot.use(session());
 bot.use(stage.middleware());
 
+//Scene
 const login = new Scene('login');
 const password = new Scene('password');
+const mainMenu = new Scene('mainMenu');
 
 // Create scene manager
 stage.register(login);
 stage.register(password);
+stage.register(password);
+stage.register(mainMenu);
 
 bot.start((ctx) => {
-    ctx.reply(`Welcome`);
-    ctx.scene.enter('login');
+    ctx.reply(`Welcome, ${ctx.from.first_name}`).then(()=>ctx.scene.enter('login'));
 });
+
 bot.catch((err, ctx) => {
     console.log(`Ooops, ecountered an error for ${ctx.updateType}`, err)
 });
 
 //Login scene
 login.enter((ctx) => {
-    ctx.reply(`${ctx.from.first_name}, enter your login: `);
+    ctx.reply(`Enter your login: `);
 });
 login.on('text', (ctx) => {
     ctx.session.login = ctx.message.text;
@@ -60,25 +59,76 @@ login.on('text', (ctx) => {
 
 //password scene
 password.enter((ctx) => {
-    ctx.reply(`${ctx.session.login}, enter your password: `);
+    ctx.reply(`Enter your password: `);
 });
 password.on('text',async (ctx) => {
     ctx.session.password = ctx.message.text;
-    let query = `SELECT Id, Name, Email FROM Contact WHERE Email =` + `'` + `${ctx.session.login}` + `'` + ` AND Password__c =` + `'` + `${ctx.session.password}` + `'`;
-    org.query({ query: query }, async (err, resp) => {
-        if (!err && resp.records) {
+    const userId = ctx.message.from.id;
+    let query = `SELECT Id, Name, Email FROM Contact WHERE Email = '${ctx.session.login}' AND Password__c = '${ctx.session.password}'`;
+    conectOrgSF.query({ query: query }, async (err, resp) => {
+        if (!err && resp.records.length != 0) {
             let contact = JSON.parse(JSON.stringify(resp.records[0]));
-            ctx.session.userId = contact.Id;
-            ctx.session.userName = contact.Name;
-            ctx.reply(`Success!`).then(()=>ctx.scene.enter('login'));
+            state[userId] = { id : userId };
+            state[userId].contactId = contact.id;
+            state[userId].name = contact.name;
+            ctx.reply(`Authorization was successful!`).then(()=>ctx.scene.enter('mainMenu'));
         } else {
             ctx.reply('Invalid login or password!!!\nTry again!').then(()=>ctx.scene.enter('login'));
         }
     });
 });
+
+//mainMenu scene
+mainMenu.enter((ctx) => {
+    ctx.reply(`${state[ctx.message.from.id].name}, select action:`,
+        Markup.inlineKeyboard([
+            Markup.callbackButton(`Current Balance`,  `Balance`),
+            Markup.callbackButton(`Create Card`, `Card`)
+        ]).extra());
+});
+
+mainMenu.on('callback_query', (ctx) => {
+    let button = ctx.callbackQuery.data;
+    switch (button) {
+        case 'Balance':
+            let current_date = new Date();
+            let query = `SELECT Id, MonthDate__c, SpentAmount__c, Balance__c, Keeper__c 
+                            FROM MonthlyExpense__c 
+                            WHERE CALENDAR_YEAR(Month_Date__c) = ${current_date.getFullYear()} AND Keeper__c ='${state[ctx.from.id].id}'`;
+            conectOrgSF.query({ query: query }, async (err, resp) => {
+                let monthlyExpenseList = JSON.parse(JSON.stringify(resp.records));
+                let totalIncome = 0;
+                let totalAmount = 0;
+                for (let i = 0; i < monthlyExpenseList.length; i++) {
+                    if (monthlyExpenseList[i].balance__c !== undefined) {
+                        totalIncome += monthlyExpenseList[i].balance__c * 100;
+                    }
+                    if (monthlyExpenseList[i].spent_amount__c !== undefined && monthlyExpenseList[i].spent_amount__c != null) {
+                        totalAmount += monthlyExpenseList[i].spent_amount__c * 100;
+                    }
+                }
+                let totalBalance = (totalIncome - totalAmount) / 100;
+                ctx.reply('Your balance: ' + totalBalance + '. Today ' + formatDate).then(()=>mainDialog(ctx, data.userId));
+            });
+            break;
+        case 'Card':
+
+            break;
+    }
+});
+
+
+
 bot.help((ctx) => ctx.reply('Send me a sticker'));
 bot.on('sticker', (ctx) => ctx.reply('👍'));
 bot.hears('hi', (ctx) => ctx.reply('Hey there'));
-bot.launch();
+
+async function startup() {
+    await bot.launch();
+    console.log(new Date(), 'Bot started as', bot.options.username);
+};
+
+startup();
 
 
+setInterval(helper.getHttp(), 900000);
